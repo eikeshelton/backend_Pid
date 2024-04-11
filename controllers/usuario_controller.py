6e0cc7cb27d092beaa1e21319f9346e02ac2f5b2
eike <eike_shelton_@hotmail.com>
2024-04-11 10:19:34 -0300

    função atualizar usuario

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -5,6 +5,7 @@ from fastapi import HTTPException
 from models.usuario import Usuario
 from dependencies import get_db
 
+
 def criar_usuario(db: Session, usuario_create):
     db_usuario = Usuario(**usuario_create.dict())
 
@@ -19,3 +20,17 @@ def login_usuario(db: Session, login: str, senha: str):
     if usuario is None:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return usuario
+
+
+def atualizar_usuario(db: Session, email: str, usuario_update: Usuario):
+    db_usuario = db.query(Usuario).filter(Usuario.email == email).first()
+    if db_usuario:
+        # Atualizar os campos do usuário com base nos dados fornecidos
+        for key, value in usuario_update.dict(exclude_unset=True).items():
+            setattr(db_usuario, key, value)
+        db.commit()
+        db.refresh(db_usuario)
+        return db_usuario
+    else:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    
--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from models.usuario import Usuario
-from controllers.usuario_controller import criar_usuario, login_usuario
+from controllers.usuario_controller import criar_usuario, login_usuario,atualizar_usuario
 from dependencies import get_db
 from pydantic import BaseModel
 from datetime import date
+from typing import Optional
 
 app = FastAPI()
 
@@ -20,7 +20,15 @@ class UsuarioCreate(BaseModel):
     data_nascimento: date
     foto_perfil: bytes
     bio: str
+class Login(BaseModel):
+    login: str
+    senha: str
 
+class UsuarioUpdate(BaseModel):
+    nome_usuario: Optional[str] = None
+    foto_perfil: Optional[bytes] = None
+    bio: Optional[str] = None
+    tipo_usuario: Optional[str] = None
 # Rota para criar um novo usuário
 @app.post("/usuarios/")
 def criar_novo_usuario(usuario_create: UsuarioCreate, db: Session = Depends(get_db)):
@@ -28,5 +36,10 @@ def criar_novo_usuario(usuario_create: UsuarioCreate, db: Session = Depends(get_
 
 # Rota para login de usuário
 @app.post("/login/")
-def fazer_login(login: str, senha: str, db: Session = Depends(get_db)):
-    return login_usuario(db, login, senha)
+def fazer_login(login_data: Login, db: Session = Depends(get_db)):
+    usuario = login_usuario(db, login_data.login, login_data.senha)
+    return login_usuario(db, login_data.login, login_data.senha)
+
+@app.put("/usuarios/{email}")
+def atualizar_dados_usuario(email: str, usuario_update: UsuarioUpdate, db: Session = Depends(get_db)):
+    return atualizar_usuario(db, email, usuario_update)
