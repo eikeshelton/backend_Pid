2579b6f96433f7b36fb95a924c060bc9eab7046e
eikeshelton <eikeshelton3@gmail.com>
2024-05-17 13:33:21 -0300

    alteraçoes

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -160,8 +160,8 @@ def alterar_senha(db: Session, email: str, nova_senha: str):
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     
 def enviar_email(destinatario: str, token: str):
-    remetente = "eike_shelton_@hotmail.com"  # Insira seu endereço de e-mail
-    senha = "99172540eike"  # Insira sua senha
+    remetente = ""  # Insira seu endereço de e-mail
+    senha = ""  # Insira sua senha
 
     # Configurar mensagem
     msg = MIMEMultipart()
