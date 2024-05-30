641bf62d41d6e2d859c9c87fe07fad3c8912f949
eikeshelton <eikeshelton3@gmail.com>
2024-05-30 18:22:37 -0300

    alteraçoes eike 30/05/24

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -1,6 +1,7 @@
 # controllers/usuario_controller.py
 import bcrypt
 import secrets
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 from fastapi import HTTPException,Depends
 from models.usuario import Usuario
@@ -205,11 +206,36 @@ def registrar_pesquisa(db: Session, texto_pesquisa: str):
     db.add(nova_pesquisa)
     db.commit()
     db.refresh(nova_pesquisa)
-def registrar_pesquisado(db:Session,registrar_busca):
-    pesquisa =HistoricoPesquisa(usuario_id=registrar_busca.usuario_id, pesquisado_id=registrar_busca.pesquisado_id)
+def registrar_pesquisado(db: Session, registrar_busca):
+    # Verifica se já existe um registro com o conjunto de usuario_id e pesquisado_id
+    pesquisa_existente = db.query(HistoricoPesquisa).filter(
+        and_(
+            HistoricoPesquisa.usuario_id == registrar_busca.usuario_id,
+            HistoricoPesquisa.pesquisado_id == registrar_busca.pesquisado_id
+        )
+    ).first()
+
+    # Se o registro já existe, retorna-o
+    if pesquisa_existente:
+        return pesquisa_existente
+    
+    # Se não existe, cria um novo registro
+    pesquisa = HistoricoPesquisa(
+        usuario_id=registrar_busca.usuario_id,
+        pesquisado_id=registrar_busca.pesquisado_id
+    )
     db.add(pesquisa)
     db.commit()
     db.refresh(pesquisa)
     return pesquisa
-def buscar_pesquisado(db: Session, pesquisado_id: int):
-    return db.query(Usuario).filter(Usuario.id == pesquisado_id).first()
+def buscar_pesquisado(db: Session, usuario_id: int,limite: int = 4)-> list[Usuario]:
+    # Busca os primeiros 5 registros em HistoricoPesquisa que correspondem ao usuario_id
+    pesquisas = db.query(HistoricoPesquisa).filter(HistoricoPesquisa.usuario_id == usuario_id).limit(limite).all()
+
+    # Coleta os IDs dos pesquisados
+    pesquisado_ids = [pesquisa.pesquisado_id for pesquisa in pesquisas]
+
+    # Busca os usuários correspondentes aos pesquisado_ids
+    usuarios = db.query(Usuario).filter(Usuario.id.in_(pesquisado_ids)).all()
+    
+    return usuarios
---
+++ models/chat.py
@@ -0,0 +1,25 @@
+# models/chat.py
+import datetime
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
+from sqlalchemy.orm import relationship
+from models.aadeclarative_base import Base
+
+class Chat(Base):
+    __tablename__ = "chat"
+    id = Column(Integer, primary_key=True, index=True)
+    remetente_id = Column(Integer, ForeignKey("usuario.id"))
+    destinatario_id = Column(Integer, ForeignKey("usuario.id"))
+    texto = Column (String)
+    data_envio = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
+    id_conversa=Column(Integer)
+
+    remetente = relationship("Usuario", foreign_keys=[remetente_id])
+    destinatario = relationship("Usuario", foreign_keys=[destinatario_id])
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "remetente_id": self.remetente_id,
+            "destinatario_id": self.destinatario_id,
+            "texto": self.texto,
+            # Outros campos que você deseja incluir no dicionário...
+        }
