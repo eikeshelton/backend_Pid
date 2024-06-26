a942c88e0c4b5c570e9c956127ba7a3b03c55787
eikeshelton <eikeshelton3@gmail.com>
2024-06-26 18:50:26 -0300

    alteracoes eike 26/06/24

---
+++ controllers/estados_municipios/municipios.py
@@ -0,0 +1,9 @@
+from models.municipio.municipio import Municipio
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
+def get_municipio_id(db: Session, id_cidade: int, estado_id: int):
+    municipio = db.query(Municipio).filter(Municipio.codigo_ibge == id_cidade, Municipio.estado_id == estado_id).first()
+    if not municipio:
+        raise HTTPException(status_code=404, detail=f"Município {id_cidade} não encontrado no estado {estado_id}")
+    return municipio.codigo_ibge
